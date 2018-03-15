3030690a7f27d7bbd404bd023533129cfed4bfa2
elaine01 <ecumming.g@gmail.com>
2018-03-14 22:02:44 -0700

    combined two tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,58 +1,48 @@
-INSERT INTO users (username, email, sell_items, buy_items)
-VALUES ('Pink', 'pink@', true, false);
-INSERT INTO users (username, email, sell_items, buy_items)
-VALUES ('lemonadelover', 'lemonadelover@', true, false);
-INSERT INTO users (username, email, sell_items, buy_items)
-VALUES ('special123', 'special123@', true, true);
-INSERT INTO users (username, email, sell_items, buy_items)
-VALUES ('Simply Lemonade', 'corporate@', true, false);
 
-
-
-INSERT INTO lemonades (product_name, price, quantity, description, image, sold, seller_id)
+INSERT INTO lemonades (product_name, price, description, image, sold, username)
 VALUES (
 	'Simply Lemonade',
 	14.69,
-	89,
 	'Sweet lemonade drink',
 	'https://images-na.ssl-images-amazon.com/images/I/41dP1bmZAPL._SL160_.jpg',
 	false,
-	1);
-INSERT INTO lemonades (product_name, price, quantity, description, image, sold, seller_id)
+	'simply123'
+);
+INSERT INTO lemonades (product_name, price, description, image, sold, username)
 VALUES (
 	'Premium Strawberry Lemonade',
 	 4.85,
-	 59,
 	 'No artificial sweeteners',
 	 'https://i5.walmartimages.com/asr/4b99c900-0442-496e-8d1f-10501f90154d_1.d0a6853baf9f91a059a4425dd06ba0cc.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
 	 false,
-	 2);
-INSERT INTO lemonades (product_name, price, quantity, description, image, sold, seller_id)
+	 'lemonadelover'
+);
+INSERT INTO lemonades (product_name, price, description, image, sold, username)
 VALUES (
 	'Green Tea Lemonade',
 	4.95,
-	16,
 	'Mildly sweet lemonade',
 	'https://globalassets.starbucks.com/assets/a2b8437c14174fa38a45280d52e92b22.jpg',
 	false,
-	3);
-INSERT INTO lemonades (product_name, price, quantity, description, image, sold, seller_id)
+	'Amzon'
+);
+INSERT INTO lemonades (product_name, price, description, image, sold, username)
 VALUES (
 	'Lipton iced tea',
 	12.00,
-	70,
 	'Natural flavor',
 	'https://images-na.ssl-images-amazon.com/images/I/91sBXCUaibL._SX522_.jpg',
 	false,
-	4
+	'Pink'
 );
-INSERT INTO lemonades (product_name, price, quantity, description, image, sold, seller_id)
+INSERT INTO lemonades (product_name, price, description, image, sold, username)
 VALUES (
 	'Country Time Pink Lemonade',
 	82.5,
 	'No artifical sweeteners or flavors',
 	'https://images-na.ssl-images-amazon.com/images/I/71zJYGkweiL._SX522_.jpg',
 	false,
-	4);
+	'jdoe23'
+);
 
 
